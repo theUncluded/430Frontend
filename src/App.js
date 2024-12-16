a16ded7d11acc63a6985ad4332905e66c826270f
cameronvit <121767628+cameronvit@users.noreply.github.com>
2024-12-16 10:25:44 -0500

    Navbar Update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './Header';
+import Header from './Navbar';
 import ProductList from './ProductList';
 import ProductGrid from './ProductGrid';
 import AdminPanel from './AdminPanel';
